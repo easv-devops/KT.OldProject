e73db93c389535d9f6467a3e09f0058381d6d572
brunstighanhval <113671720+brunstighanhval@users.noreply.github.com>
2023-11-21 09:30:04 +0100

    customerbuyService

--- infrastructure/Repositories/CustomerBuyRepository.cs
+++ infrastructure/Repositories/CustomerBuyRepository.cs
@@ -27,6 +27,32 @@ public class CustomerBuyRepository
     {
         var sql =
             @"INSERT INTO customer_by_id (order_id, avatar_id) VALUES (@order_id, @avatar_id) RETURNING *;";
+
+        using (var conn = _dataSource.OpenConnection())
+        {
+            return conn.QueryFirst<CustomerBuy>(sql, new { order_id, avatar_id });
+        }
+    }
+
+    public CustomerBuy UpdateCustomerBuy(int customer_buy_id, int order_id, int avatar_id)
+    {
+        var sql =
+            @"UPDATE customer_buy SET order_id = @order_id, avatar_id = @avatar_id RETURNING *;";
+
+        using (var conn = _dataSource.OpenConnection())
+        {
+            return conn.QueryFirst<CustomerBuy>(sql, new {customer_buy_id, order_id, avatar_id});
+        }
+    }
+
+    public void DeleteCustomerBuy(int customer_buy_id)
+    {
+        var sql = @"DELETE FROM customer_buy WHERE customer_buy_id = @customer_buy_id RETURNING *;";
+
+        using (var conn = _dataSource.OpenConnection())
+        {
+            conn.QueryFirst<CustomerBuy>(sql, new {customer_buy_id});
+        }
     }
     
 }
