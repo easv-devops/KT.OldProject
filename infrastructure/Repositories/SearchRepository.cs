f408f02ed15593e2bfacbffad2cec5a4bb998a9f
brunstighanhval <113671720+brunstighanhval@users.noreply.github.com>
2023-11-24 10:49:45 +0100

    Search

---
+++ infrastructure/Repositories/SearchRepository.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using infrastructure.DataModels;
+using Npgsql;
+
+namespace infrastructure.Repositories;
+
+public class SearchRepository
+{
+    private NpgsqlDataSource _dataSource;
+
+    public SearchRepository(NpgsqlDataSource dataSource)
+    {
+       _dataSource = dataSource; 
+    }
+
+    public IEnumerable<Avatar> SearchAvatar(string searchQuery)
+    {
+        var sql = $@"SELECT * FROM account.avatar WHERE avatar_name ILIKE '%{searchQuery}%';";
+
+        using (var conn = _dataSource.OpenConnection())
+        {
+            return conn.Query<Avatar>(sql, new { searchQuery });
+        }
+    }
+}
---
+++ service/Services/SearchService.cs
@@ -0,0 +1,19 @@
+﻿using infrastructure.DataModels;
+using infrastructure.Repositories;
+
+namespace service.Services;
+
+public class SearchService
+{
+    private readonly SearchRepository _searchRepository;
+
+    public SearchService(SearchRepository searchRepository)
+    {
+        _searchRepository = searchRepository;
+    }
+
+    public IEnumerable<Avatar> SearchAvatar(string searchQuery)
+    {
+        return _searchRepository.SearchAvatar(searchQuery);
+    }
+}
