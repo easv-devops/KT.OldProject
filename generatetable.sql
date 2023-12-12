16f9d68049b3eaa388475e15a5928456e44c3317
brunstighanhval <113671720+brunstighanhval@users.noreply.github.com>
2023-12-12 10:01:22 +0100

    sql

--- generatetable.sql
+++ generatetable.sql
@@ -25,7 +25,26 @@ create table webshop.avatar
 );
 
 INSERT INTO webshop.avatar (avatar_name,avatar_price, information, deleted)
-VALUES ('Nummer1', 10, 'Her er lidt information', false);
+VALUES ('Dwayne “The Rock” Johnson', 10, 'Dwayne Johnson, also known as “The Rock,” is a multifaceted celebrity. He’s a former professional wrestler, a successful actor, and a charismatic personality. With a strong presence on social media, Johnson engages with his fans regularly. He’s known for his roles in blockbuster films like “Fast & Furious” and “Jumanji.”', false);
+INSERT INTO webshop.avatar (avatar_name,avatar_price, information, deleted)
+VALUES ('Beyoncé', 10, 'Beyoncé Knowles-Carter is an iconic singer, songwriter, and actress. As one of the most influential musicians of our time, she has a dedicated global fan base known as the “Beyhive.” Beyoncé’s albums and live performances consistently receive critical acclaim.', false);
+INSERT INTO webshop.avatar (avatar_name,avatar_price, information, deleted)
+VALUES ('Cristiano Ronaldo', 10, 'Cristiano Ronaldo is one of the greatest football (soccer) players in history. His skills on the field and his social media presence make him a global sensation. Ronaldo has won numerous awards and holds various records in football.', false);
+INSERT INTO webshop.avatar (avatar_name,avatar_price, information, deleted)
+VALUES ('Kim Kardashian', 10, 'Kim Kardashian rose to fame through the reality TV show “Keeping Up with the Kardashians.” She’s also a businesswoman, model, and socialite. Kim’s influence extends to fashion, beauty, and lifestyle.', false);
+INSERT INTO webshop.avatar (avatar_name,avatar_price, information, deleted)
+VALUES ('Ellen DeGeneres', 10, 'Ellen DeGeneres is a beloved comedian, actress, and television host. Her talk show, “The Ellen DeGeneres Show,” has been a source of entertainment and inspiration for millions worldwide.', false);
+INSERT INTO webshop.avatar (avatar_name,avatar_price, information, deleted)
+VALUES ('Lionel Messi', 10, 'Lionel Messi, like Cristiano Ronaldo, is considered one of the greatest football players ever. He’s known for his incredible dribbling skills and goal-scoring abilities. Messi’s name is synonymous with excellence in football.', false);
+INSERT INTO webshop.avatar (avatar_name,avatar_price, information, deleted)
+VALUES ('Oprah Winfrey', 10, 'Oprah Winfrey, often referred to as the “Queen of All Media,” is a media mogul, talk show host, and philanthropist. Her influence in the entertainment industry and her charitable work have made her a global icon.', false);
+INSERT INTO webshop.avatar (avatar_name,avatar_price, information, deleted)
+VALUES ('Justin Bieber', 10, 'Justin Bieber gained fame as a teenager through his music. He’s known for hits like “Baby” and “Sorry.” Bieber’s fan base, known as “Beliebers,” remains dedicated to his music and career.', false);
+INSERT INTO webshop.avatar (avatar_name,avatar_price, information, deleted)
+VALUES ('Rihanna', 10, 'Rihanna is a Barbadian singer, actress, and fashion designer. Her music career has been immensely successful, and she’s also made a mark in the beauty and fashion industries with her Fenty brand.', false);
+INSERT INTO webshop.avatar (avatar_name,avatar_price, information, deleted)
+VALUES ('Kylie Jenner', 10, 'Kylie Jenner, part of the Kardashian-Jenner family, has risen to prominence as a businesswoman and social media influencer. She’s known for her cosmetics company, Kylie Cosmetics.', false);
+
 
 create table webshop.order
 (
