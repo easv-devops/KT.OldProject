abaddcd60e634e4ac57bde193a7c29b2ed56c5d2
KasperTell <kaspertell@gmail.com>
2024-02-18 19:33:11 +0100

    Revert "Delete NewTests.cs"
    This reverts commit f4630061d56bfb658c38add8b68d895b046062e6.

---
+++ test/NewTests.cs
@@ -0,0 +1,22 @@
+﻿using api.Controllers;
+using NUnit.Framework;
+
+namespace Tests;
+
+[TestFixture]
+public class NewTests
+{
+    
+    private readonly AvatarController _avatarController;
+    
+    public NewTests(AvatarController avatarController)
+    {
+        _avatarController = avatarController;
+    }
+
+    [Test]
+    public void test()
+    { 
+        Assert.That(_avatarController.DoSomething(), Is.EqualTo("Hello World"));
+    }
+}
